385254a794fa1e53a67ec18189b4bfdf38e4afb0
lemphis <lemphis@gmail.com>
2024-06-12 08:36:19 +0900

    Refactor ItemReaders using abstract class and template method pattern
    * shp, shx, dbf 파일을 처리할 abstract class 생성
    * polygon 데이터를 처리할 각 ItemReader는 abstract class를 상속하여 dto 변환 로직만 처리하도록 수정

---
+++ src/main/kotlin/me/lemphis/realestatepublicbatch/gis/ShapeFileItemReader.kt
@@ -0,0 +1,54 @@
+package me.lemphis.realestatepublicbatch.gis
+
+import org.geotools.api.data.DataStore
+import org.geotools.api.feature.simple.SimpleFeature
+import org.geotools.data.shapefile.ShapefileDataStore
+import org.geotools.data.simple.SimpleFeatureIterator
+import org.springframework.batch.item.ExecutionContext
+import org.springframework.batch.item.file.ResourceAwareItemReaderItemStream
+import org.springframework.core.io.Resource
+import java.nio.charset.Charset
+
+abstract class ShapeFileItemReader<T> : ResourceAwareItemReaderItemStream<T> {
+	private lateinit var resource: Resource
+	private lateinit var dataStore: DataStore
+	private lateinit var featureIterator: SimpleFeatureIterator
+	private val readCountKey = "read.count"
+	private var readCount = 0
+
+	override fun setResource(resource: Resource) {
+		this.resource = resource
+		this.readCount = 0
+	}
+
+	override fun open(executionContext: ExecutionContext) {
+		this.dataStore = ShapefileDataStore(resource.url).apply { charset = Charset.forName("EUC-KR") }
+		this.featureIterator = (this.dataStore as ShapefileDataStore).featureSource.features.features()
+		if (executionContext.containsKey(readCountKey)) {
+			this.readCount = executionContext.getInt(readCountKey)
+			for (i in 0 until readCount) {
+				featureIterator.next()
+			}
+		}
+	}
+
+	override fun update(executionContext: ExecutionContext) {
+		executionContext.putInt(readCountKey, this.readCount)
+	}
+
+	override fun close() {
+		featureIterator.close()
+		dataStore.dispose()
+	}
+
+	override fun read(): T? {
+		if (featureIterator.hasNext()) {
+			++readCount
+			return toShapeFileSpec(featureIterator.next())
+		}
+		return null
+	}
+
+	abstract fun toShapeFileSpec(feature: SimpleFeature): T
+
+}
--- src/main/kotlin/me/lemphis/realestatepublicbatch/gis/buildingpolygon/BuildingPolygonFileItemReader.kt
+++ src/main/kotlin/me/lemphis/realestatepublicbatch/gis/buildingpolygon/BuildingPolygonFileItemReader.kt
@@ -1,18 +1,12 @@
 package me.lemphis.realestatepublicbatch.gis.buildingpolygon
 
-import org.geotools.api.data.DataStore
+import me.lemphis.realestatepublicbatch.gis.ShapeFileItemReader
 import org.geotools.api.feature.simple.SimpleFeature
-import org.geotools.data.shapefile.ShapefileDataStore
-import org.geotools.data.simple.SimpleFeatureIterator
 import org.locationtech.jts.geom.MultiPolygon
-import org.springframework.batch.item.ExecutionContext
-import org.springframework.batch.item.file.ResourceAwareItemReaderItemStream
 import org.springframework.batch.item.file.builder.MultiResourceItemReaderBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.Resource
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
-import java.nio.charset.Charset
 import java.util.Date
 
 @Configuration
@@ -25,79 +19,39 @@ class BuildingPolygonFileItemReader {
 		.delegate(buildingPolygonFileItemReaderDelegate())
 		.build()
 
-	private fun buildingPolygonFileItemReaderDelegate() =
-		object : ResourceAwareItemReaderItemStream<BuildingPolygonFileSpec> {
-			private lateinit var resource: Resource
-			private lateinit var dataStore: DataStore
-			private lateinit var featureIterator: SimpleFeatureIterator
-			private val readCountKey = "read.count"
-			private var readCount = 0
-
-			override fun setResource(resource: Resource) {
-				this.resource = resource
-				this.readCount = 0
-			}
-
-			override fun open(executionContext: ExecutionContext) {
-				this.dataStore = ShapefileDataStore(resource.url).apply { charset = Charset.forName("EUC-KR") }
-				this.featureIterator = (this.dataStore as ShapefileDataStore).featureSource.features.features()
-				if (executionContext.containsKey(readCountKey)) {
-					this.readCount = executionContext.getInt(readCountKey)
-					for (i in 0 until readCount) {
-						featureIterator.next()
-					}
-				}
-			}
-
-			override fun update(executionContext: ExecutionContext) {
-				executionContext.putInt(readCountKey, this.readCount)
-			}
-
-			override fun close() {
-				featureIterator.close()
-				dataStore.dispose()
-			}
-
-			override fun read(): BuildingPolygonFileSpec? {
-				if (featureIterator.hasNext()) {
-					++readCount
-					return toBuildingPolygonSpec(featureIterator.next())
-				}
-				return null
-			}
-
-			fun toBuildingPolygonSpec(feature: SimpleFeature) = BuildingPolygonFileSpec(
-				polygon = feature.getAttribute("the_geom") as MultiPolygon,
-				원천도형ID = feature.getAttribute("A0") as Int,
-				GIS건물통합식별번호 = feature.getAttribute("A1") as String,
-				고유번호 = feature.getAttribute("A2") as String,
-				법정동코드 = feature.getAttribute("A3") as String,
-				법정동명 = feature.getAttribute("A4") as String,
-				지번 = feature.getAttribute("A5") as String,
-				특수지코드 = feature.getAttribute("A6") as String,
-				특수지구분명 = feature.getAttribute("A7") as String,
-				건축물용도코드 = feature.getAttribute("A8") as String?,
-				건축물용도명 = feature.getAttribute("A9") as String,
-				건축물구조코드 = feature.getAttribute("A10") as String?,
-				건축물구조명 = feature.getAttribute("A11") as String,
-				건축물면적 = feature.getAttribute("A12") as Double?,
-				사용승인일자 = feature.getAttribute("A13") as String,
-				연면적 = feature.getAttribute("A14") as Double,
-				대지면적 = feature.getAttribute("A15") as Double?,
-				높이 = feature.getAttribute("A16") as Double?,
-				건폐율 = feature.getAttribute("A17") as Double?,
-				용적율 = feature.getAttribute("A18") as Double?,
-				건축물ID = feature.getAttribute("A19") as String?,
-				위반건축물여부 = feature.getAttribute("A20") as String,
-				참조체계연계키 = feature.getAttribute("A21") as String?,
-				데이터기준일자 = feature.getAttribute("A22") as Date,
-				원천시도시군구코드 = feature.getAttribute("A23") as String,
-				건물명 = feature.getAttribute("A24") as String?,
-				건물동명 = feature.getAttribute("A25") as String?,
-				지상층수 = feature.getAttribute("A26") as Int?,
-				지하층수 = feature.getAttribute("A27") as Int?,
-				데이터생성변경일자 = feature.getAttribute("A28") as String,
-			)
-		}
+	private fun buildingPolygonFileItemReaderDelegate() = object : ShapeFileItemReader<BuildingPolygonFileSpec>() {
+		override fun toShapeFileSpec(feature: SimpleFeature) = BuildingPolygonFileSpec(
+			polygon = feature.getAttribute("the_geom") as MultiPolygon,
+			원천도형ID = feature.getAttribute("A0") as Int,
+			GIS건물통합식별번호 = feature.getAttribute("A1") as String,
+			고유번호 = feature.getAttribute("A2") as String,
+			법정동코드 = feature.getAttribute("A3") as String,
+			법정동명 = feature.getAttribute("A4") as String,
+			지번 = feature.getAttribute("A5") as String,
+			특수지코드 = feature.getAttribute("A6") as String,
+			특수지구분명 = feature.getAttribute("A7") as String,
+			건축물용도코드 = feature.getAttribute("A8") as String?,
+			건축물용도명 = feature.getAttribute("A9") as String,
+			건축물구조코드 = feature.getAttribute("A10") as String?,
+			건축물구조명 = feature.getAttribute("A11") as String,
+			건축물면적 = feature.getAttribute("A12") as Double?,
+			사용승인일자 = feature.getAttribute("A13") as String,
+			연면적 = feature.getAttribute("A14") as Double,
+			대지면적 = feature.getAttribute("A15") as Double?,
+			높이 = feature.getAttribute("A16") as Double?,
+			건폐율 = feature.getAttribute("A17") as Double?,
+			용적율 = feature.getAttribute("A18") as Double?,
+			건축물ID = feature.getAttribute("A19") as String?,
+			위반건축물여부 = feature.getAttribute("A20") as String,
+			참조체계연계키 = feature.getAttribute("A21") as String?,
+			데이터기준일자 = feature.getAttribute("A22") as Date,
+			원천시도시군구코드 = feature.getAttribute("A23") as String,
+			건물명 = feature.getAttribute("A24") as String?,
+			건물동명 = feature.getAttribute("A25") as String?,
+			지상층수 = feature.getAttribute("A26") as Int?,
+			지하층수 = feature.getAttribute("A27") as Int?,
+			데이터생성변경일자 = feature.getAttribute("A28") as String,
+		)
+	}
 
 }
